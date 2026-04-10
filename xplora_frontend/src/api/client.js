7a18217a14a330f5a2e653235242b1dc3efa26d5
yash_goswami <yashgoswami000000@gmail.com>
2026-04-11 01:46:53 +0530

    Use VITE_API_URL env var for backend URL

--- xplora_frontend/src/api/client.js
+++ xplora_frontend/src/api/client.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
   },
