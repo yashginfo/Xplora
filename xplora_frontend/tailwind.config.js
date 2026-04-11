d521ad79b8cef65b26cc2d090e3a72dcd0088306
yash_goswami <yashgoswami000000@gmail.com>
2026-04-11 12:52:57 +0530

    FixedCsstailwind

---
+++ xplora_frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+export default {
+  theme: {
+    extend: {
+      height: {
+        screen: '100dvh',   // h-screen now = 100dvh everywhere
+      },
+      minHeight: {
+        screen: '100dvh',   // min-h-screen now = 100dvh everywhere
+      },
+    },
+  },
+}
